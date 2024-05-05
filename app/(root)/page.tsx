c58a65cfeef881ac75c46f95ae082ab8b3e5ab64
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-05-05 11:58:16 +0300

    home page ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Mugambi Javan'};
   return (
-    <div> Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox 
+            type= "greeting"
+            title= "Welcome"
+            user= {loggedIn?.firstName || 'User'}
+            subtext= "Access your Bank accounts"
+          />
+
+          <TotalBalanceBox 
+            accounts= {[]}
+            totalBanks= {1}
+            totalCurrentBalance= {10000}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
